fcc3f7d5d45b625d6ac8ad81a72a8a83af1b2acb
penekhun <penekhun@gmail.com>
2022-05-08 19:42:54 +0900

    JWT관련 파일 생성

---
+++ src/main/java/com/penekhun/ctfjserver/Config/Jwt/JwtSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.penekhun.ctfjserver.Config.Jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        //UserNamePassWord 필터 전에 JwtFilter를 추가
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
