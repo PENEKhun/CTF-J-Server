229a2a9dea2a957298b2b0fca5aaf14de47f6992
penekhun <penekhun@gmail.com>
2022-05-08 19:41:03 +0900

    Account JPA 생성

---
+++ src/main/java/com/penekhun/ctfjserver/User/Repository/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.penekhun.ctfjserver.User.Repository;
+
+import com.penekhun.ctfjserver.User.Entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    public Optional<Account> findByUsername(String username);
+    public Optional<Account> findOneByUsername(String username);
+    public Optional<Account> findOneByEmail(String email);
+    public Optional<Account> findOneByNickname(String nickname);
+
+}
