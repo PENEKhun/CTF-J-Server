f16a75fbd842f0dd0ccd6ea21701cfa6ac7c7307
penekhun <penekhun@gmail.com>
2022-05-08 22:17:14 +0900

    AccountDto 빌더, Valid 적용

--- src/main/java/com/penekhun/ctfjserver/User/Dto/AccountDto.java
+++ src/main/java/com/penekhun/ctfjserver/User/Dto/AccountDto.java
@@ -1,25 +1,42 @@
 package com.penekhun.ctfjserver.User.Dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Column;
-import javax.persistence.Lob;
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
+@RequiredArgsConstructor
 public class AccountDto {
 
-
-    public class Req {
+    public static class Req{
 
         @Data
-        public class Signup{
+        @Valid
+        public static class Signup{
+            @Length(min = 3, max = 30)
             private String username;
+
+            @Length(min = 8, max = 20)
             private String password;
+
+            @Length(min = 3, max = 20)
             private String nickname;
+
+            @Email
             private String email;
+
+            @Length(min = 2, max = 10)
             private String realName;
-            //private String userRole;
+
+            @Builder
+            public Signup(String username, String password, String nickname, String email, String realName) {
+                this.username = username;
+                this.password = password;
+                this.nickname = nickname;
+                this.email = email;
+                this.realName = realName;
+            }
         }
 
     }
