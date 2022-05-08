3cfd46981cc887d518f82b6872b3511a9b6052c8
penekhun <penekhun@gmail.com>
2022-05-08 19:43:52 +0900

    CustomUser 생성 (SpringSecurity.User)

---
+++ src/main/java/com/penekhun/ctfjserver/Config/SecurityUser.java
@@ -0,0 +1,24 @@
+package com.penekhun.ctfjserver.Config;
+
+import com.penekhun.ctfjserver.User.Entity.Account;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+@Slf4j
+public class SecurityUser extends User {
+
+    private Account member;
+
+    public SecurityUser(Account account) {
+        super(account.getUsername(), account.getPassword(), AuthorityUtils.createAuthorityList(account.getUserRole()));
+
+
+        log.info("SecurityUser account.uid = {}", account.getId());
+        log.info("SecurityUser account.username = {}", account.getUsername());
+        log.info("SecurityUser account.password = {}", account.getPassword());
+        log.info("SecurityUser account.role = {}", account.getUserRole());
+
+        this.member = account;
+    }
+}
