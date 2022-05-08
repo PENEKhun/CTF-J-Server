8e88fa2e108765558237204b410cbdfe8e0736fc
penekhun <penekhun@gmail.com>
2022-05-08 22:17:37 +0900

    first commit

---
+++ src/test/java/com/penekhun/ctfjserver/User/Controller/AccountControllerTest.java
@@ -0,0 +1,16 @@
+package com.penekhun.ctfjserver.User.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Test
+    void 테스트postMapping() {
+
+    }
+}
---
+++ src/test/java/com/penekhun/ctfjserver/User/Service/AccountServiceTest.java
@@ -0,0 +1,18 @@
+package com.penekhun.ctfjserver.User.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceTest {
+
+    @Test
+    void login() {
+
+    }
+
+    @Test
+    void signup() {
+
+    }
+}
