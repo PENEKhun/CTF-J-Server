221e1c5cd7789afce418da626308007cde0e35ca
penekhun <penekhun@gmail.com>
2022-05-09 18:26:42 +0900

    기본 테스트코드 작성

--- src/test/java/com/penekhun/ctfjserver/User/Controller/LoginOutControllerTest.java
+++ src/test/java/com/penekhun/ctfjserver/User/Controller/LoginOutControllerTest.java
@@ -1,30 +1,25 @@
 package com.penekhun.ctfjserver.User.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.penekhun.ctfjserver.Config.SecurityConfig;
 import com.penekhun.ctfjserver.User.Dto.AccountDto;
 import com.penekhun.ctfjserver.forTest.MultiValueMapConverter;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import javax.annotation.security.RunAs;
 import javax.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
@@ -34,31 +29,57 @@ class LoginOutControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void 테스트로그인() {
+    private final AccountDto.Req.Signup signup =
+            AccountDto.Req.Signup.builder()
+            .username("test9838test")
+            .password("test9838test")
+            .nickname("TestAccount9838")
+            .email("testaccount@test.com")
+            .realName("홍길동")
+            .build();
 
+    @Test
+    void 테스트로그인_성공() throws Exception {
+        //todo: dto 변경
+        MultiValueMap<String, String> multiValueSignup = MultiValueMapConverter.convert(new ObjectMapper(), signup);
+
+        mockMvc.perform(
+                post("/api/v1/login").params(multiValueSignup))
+                .andExpect(status().isOk())
+                .andExpect(header().exists("Authorization"))
+                .andDo(print());
     }
+
+
+
+    @Test
+    void 테스트로그인_실패() throws Exception {
+        //todo: dto 변경
+        MultiValueMap<String, String> loginParams = new LinkedMultiValueMap<>();
+        loginParams.add("username", "sifjicmzxwci21");
+        loginParams.add("password", "password111111");
+
+        Assertions.assertThrows(NullPointerException.class, ()-> {
+                    mockMvc.perform(post("/api/v1/login").params(loginParams))
+                            .andExpect(status().is5xxServerError()); });
+    }
+                  //  .andExpect(status().is5xxServerError())
+                  //  .andExpect(result -> assertTrue(result.getResolvedException() instanceof java.lang.NullPointerException))
 
     @Test
     void 테스트로그아웃() {
+
     }
 
     @Test
+    @BeforeEach
     void 테스트임시_회원가입_컨트롤러() throws Exception {
-        AccountDto.Req.Signup signup= AccountDto.Req.Signup.builder()
-                                    .username("test9838test")
-                                    .password("test9838test")
-                                    .nickname("TestAccount9838")
-                                    .email("testaccount@test.com")
-                                    .realName("홍길동")
-                                    .build();
-
         MultiValueMap<String, String> multiValueSignup = MultiValueMapConverter.convert(new ObjectMapper(), signup);
 
         mockMvc.perform(
                 post("/api/v1/tempSignup").params(multiValueSignup))
                 .andExpect(status().isOk())
                 .andDo(print());
-
     }
+
 }
