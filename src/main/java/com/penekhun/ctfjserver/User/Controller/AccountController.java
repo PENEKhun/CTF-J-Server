8c92ca8c1ba3d7ef7b0d89015d148369d9b23843
penekhun <penekhun@gmail.com>
2022-05-10 00:18:24 +0900

    Clean

--- src/main/java/com/penekhun/ctfjserver/User/Controller/AccountController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/AccountController.java
@@ -4,15 +4,12 @@ import com.penekhun.ctfjserver.User.Dto.AccountDto;
 import com.penekhun.ctfjserver.User.Service.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/account")
