efebcb06c282b84a23f314af218e6c3497c975a6
penekhun <penekhun@gmail.com>
2022-05-10 00:17:56 +0900

    [ProblemService] 간단하게 문제 불러오는 비지니스 로직 추가

--- src/main/java/com/penekhun/ctfjserver/User/Service/ProblemService.java
+++ src/main/java/com/penekhun/ctfjserver/User/Service/ProblemService.java
@@ -1,19 +1,32 @@
 package com.penekhun.ctfjserver.User.Service;
 
 import com.penekhun.ctfjserver.User.Dto.ProblemDto;
+import com.penekhun.ctfjserver.User.Entity.Problem;
+import com.penekhun.ctfjserver.User.Repository.ProblemRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ProblemService {
+
+    private final ProblemRepository problemRepository;
 
     public ResponseEntity<String> addProblem(ProblemDto.Default problemDto){
         return null;
     }
 
     public ResponseEntity<String> getProblemList(){
+
+        List<Problem> problems = problemRepository.findAllProblem(false);
+
+        problems.forEach(a-> System.out.println(a.getFlag()));
+
         return null;
     }
 
