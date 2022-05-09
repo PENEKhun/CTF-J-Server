62651cd554ba59f430a129ec5a123bc36f3914dc
penekhun <penekhun@gmail.com>
2022-05-10 00:16:22 +0900

    first Commit

---
+++ src/main/java/com/penekhun/ctfjserver/User/Repository/ProblemRepository.java
@@ -0,0 +1,28 @@
+package com.penekhun.ctfjserver.User.Repository;
+
+import com.penekhun.ctfjserver.User.Entity.Problem;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class ProblemRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Problem problem){
+        em.persist(problem);
+    }
+
+    public List<Problem> findAllProblem(boolean includePrivate){
+        if (includePrivate)
+            return (em.createQuery("select m from Problem m", Problem.class).getResultList());
+        else
+            return (em.createQuery("select m from Problem m where m.isPublic = :isPublic", Problem.class)
+                .setParameter("isPublic", true).getResultList());
+    }
+
+}
