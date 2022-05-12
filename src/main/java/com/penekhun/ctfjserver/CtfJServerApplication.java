cbbcde8543fa4f128e37ee6a3dfde72397205245
penekhun <penekhun@gmail.com>
2022-05-12 22:42:40 +0900

    [StfJServerApplication] ModelMapper Bean등록

--- src/main/java/com/penekhun/ctfjserver/CtfJServerApplication.java
+++ src/main/java/com/penekhun/ctfjserver/CtfJServerApplication.java
@@ -1,10 +1,14 @@
 package com.penekhun.ctfjserver;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CtfJServerApplication {
+
+    private final ModelMapper modelMapper = new ModelMapper();
 
     public static void main(String[] args) {
         SpringApplication.run(CtfJServerApplication.class, args);
