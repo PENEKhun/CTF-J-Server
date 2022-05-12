673006a899e5186c32b990745b4e8a8d3b04e0c4
penekhun <penekhun@gmail.com>
2022-05-12 22:47:10 +0900

    [SecurityUser] @Setter 추가

--- src/main/java/com/penekhun/ctfjserver/Config/SecurityUser.java
+++ src/main/java/com/penekhun/ctfjserver/Config/SecurityUser.java
@@ -1,24 +1,25 @@
 package com.penekhun.ctfjserver.Config;
 
 import com.penekhun.ctfjserver.User.Entity.Account;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
 @Slf4j
+@Getter
 public class SecurityUser extends User {
 
-    private Account member;
+    private Account account;
 
     public SecurityUser(Account account) {
         super(account.getUsername(), account.getPassword(), AuthorityUtils.createAuthorityList(account.getUserRole()));
-
 
         log.info("SecurityUser account.uid = {}", account.getId());
         log.info("SecurityUser account.username = {}", account.getUsername());
         log.info("SecurityUser account.password = {}", account.getPassword());
         log.info("SecurityUser account.role = {}", account.getUserRole());
 
-        this.member = account;
+        this.account = account;
     }
 }
