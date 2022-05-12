bccc7de6521e9f056b8761cd713c1d6ebd4fd166
penekhun <penekhun@gmail.com>
2022-05-12 22:43:00 +0900

    [StfJServerApplication] BcryptoPasswordEncoder Bean등록

--- src/main/java/com/penekhun/ctfjserver/CtfJServerApplication.java
+++ src/main/java/com/penekhun/ctfjserver/CtfJServerApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CtfJServerApplication {
@@ -12,6 +14,20 @@ public class CtfJServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CtfJServerApplication.class, args);
+    }
+
+
+    @Bean //해당 메서드의 리턴되는 오브젝트를 IoC로 등록함
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
 }
