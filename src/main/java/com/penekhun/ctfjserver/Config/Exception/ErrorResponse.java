ae2a5e75dc3bc264829921026a6dbd92a45bfaff
penekhun <penekhun@gmail.com>
2022-05-12 22:43:53 +0900

    ErrorResponse 변수명 변경

--- src/main/java/com/penekhun/ctfjserver/Config/Exception/ErrorResponse.java
+++ src/main/java/com/penekhun/ctfjserver/Config/Exception/ErrorResponse.java
@@ -1,24 +1,28 @@
 package com.penekhun.ctfjserver.Config.Exception;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 @RequiredArgsConstructor
 public class ErrorResponse {
     private final LocalDateTime timestamp = LocalDateTime.now();
-    private final String code;
+    private final String errorCode;
     private final String message;
+    private final String detail;
 
     public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode){
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(ErrorResponse.builder()
-                        .code(errorCode.name())
-                        .message(errorCode.getMessage())
+                        .errorCode(errorCode.getErrorCode())
+                        .message(errorCode.name())
+                        .detail(errorCode.getMessage())
                         .build()
                 );
     }
