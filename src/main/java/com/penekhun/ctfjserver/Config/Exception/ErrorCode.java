92f48b1136d7cf18c1b664c668d5779f850e43bf
penekhun <penekhun@gmail.com>
2022-05-12 22:43:39 +0900

    ErrorCode 수정

--- src/main/java/com/penekhun/ctfjserver/Config/Exception/ErrorCode.java
+++ src/main/java/com/penekhun/ctfjserver/Config/Exception/ErrorCode.java
@@ -5,23 +5,25 @@ public enum ErrorCode {
     // Common
     INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
     METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
-    HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
+    HANDLE_ACCESS_DENIED(403, "C003", "잘못된 접근입니다."),
+    CANNOT_LOAD_USERNAME(500, "C004", "존재하지 않는 username입니다"),
 
     // Member
-    EMAIL_DUPLICATION(400, "M001", "존재하는 이메일"),
-    NICKNAME_DUPLICATION(400, "M002", "존재하는 닉네임"),
-    USERNAME_DUPLICATION(400, "M003", "존재하는 아이디"),
+    EMAIL_DUPLICATION(200, "M001", "존재하는 이메일"),
+    NICKNAME_DUPLICATION(200, "M002", "존재하는 닉네임"),
+    USERNAME_DUPLICATION(200, "M003", "존재하는 아이디"),
+    MEMBER_NOT_FOUND(200, "M004", "존재하지 않은 회원정보"),
     //LOGIN_INPUT_INVALID(400, "M002", "Login input is invalid"),
 
     ;
-    private final String code;
+    private final String errorCode;
     private final String message;
     private int status;
 
-    ErrorCode(final int status, final String code, final String message) {
+    ErrorCode(final int status, final String errorCode, final String message) {
         this.status = status;
         this.message = message;
-        this.code = code;
+        this.errorCode = errorCode;
     }
 
     public int getStatus() {
@@ -30,5 +32,9 @@ public enum ErrorCode {
 
     public String getMessage() {
         return message;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
     }
 }
