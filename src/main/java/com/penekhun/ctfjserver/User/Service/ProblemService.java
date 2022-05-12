f4caa841b2883cfc82868feaef90310bea9a1160
penekhun <penekhun@gmail.com>
2022-05-12 22:49:23 +0900

    ProblemService 문제 등록하는 간단한 로직 구현

--- src/main/java/com/penekhun/ctfjserver/User/Service/ProblemService.java
+++ src/main/java/com/penekhun/ctfjserver/User/Service/ProblemService.java
@@ -1,13 +1,16 @@
 package com.penekhun.ctfjserver.User.Service;
 
 import com.penekhun.ctfjserver.User.Dto.ProblemDto;
+import com.penekhun.ctfjserver.User.Entity.Account;
 import com.penekhun.ctfjserver.User.Entity.Problem;
 import com.penekhun.ctfjserver.User.Repository.ProblemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -15,19 +18,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProblemService {
 
-    private final ProblemRepository problemRepository;
 
-    public ResponseEntity<String> addProblem(ProblemDto.Default problemDto){
+    private final ProblemRepository problemRepository;
+    private final ModelMapper modelMapper;
+
+    @Transactional
+    public ProblemDto.Default addProblem(Account account, ProblemDto.Default problemDto){
+        log.info(problemDto.getTitle());
+        Problem problem =  modelMapper.map(problemDto, Problem.class);
+        log.info("writer is ... {}", account.getEmail());
+        problem.setAuthorId(account);
+        problemRepository.save(problem);
         return null;
     }
 
-    public ResponseEntity<String> getProblemList(){
+    public List<Problem> getProblemList(){
 
         List<Problem> problems = problemRepository.findAllProblem(false);
-
-        problems.forEach(a-> System.out.println(a.getFlag()));
-
-        return null;
+        return problems;
     }
 
     public ResponseEntity<?> authProblem(ProblemDto.Req.Auth auth){
