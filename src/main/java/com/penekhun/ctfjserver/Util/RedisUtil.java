229121c81c5621bc4e4fc8e8ec3fc950ed849562
penekhun <penekhun@gmail.com>
2022-05-17 08:00:32 +0900

    [RedisUtil] 토큰을 redis에 저장시 만기시간 지정

--- src/main/java/com/penekhun/ctfjserver/Util/RedisUtil.java
+++ src/main/java/com/penekhun/ctfjserver/Util/RedisUtil.java
@@ -2,16 +2,23 @@ package com.penekhun.ctfjserver.Util;
 
 import com.penekhun.ctfjserver.User.Entity.TokenStorage;
 import com.penekhun.ctfjserver.User.Repository.TokenStorageRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class RedisUtil {
 
     private final TokenStorageRepository refreshTokenRepository;
+    private final long refreshTokenValidityInMilliseconds;
+    
+    public RedisUtil(TokenStorageRepository refreshTokenRepository,
+                     @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.refreshTokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+    }
 
     public void insertTokenToStorage(TokenStorage refreshTokenEntity){
+        refreshTokenEntity.setExpiration(refreshTokenValidityInMilliseconds);
         refreshTokenRepository.save(refreshTokenEntity);
     }
 
