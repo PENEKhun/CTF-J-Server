f202aac5da63e094ed547930a8e7a3957a1f6ac4
penekhun <penekhun@gmail.com>
2022-05-17 10:06:32 +0900

    first commit

---
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/ConfigController.java
@@ -0,0 +1,14 @@
+package com.penekhun.ctfjserver.User.Controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/config")
+@Slf4j
+@RequiredArgsConstructor
+public class ConfigController {
+
+}
