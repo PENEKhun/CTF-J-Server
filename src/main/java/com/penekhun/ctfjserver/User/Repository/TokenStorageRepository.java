baaf308c59e8da0b5e63652c92171afc04cb44b7
penekhun <penekhun@gmail.com>
2022-05-17 10:06:52 +0900

    [TokenStorageRepository] 안쓰는 메서드 삭제

--- src/main/java/com/penekhun/ctfjserver/User/Repository/TokenStorageRepository.java
+++ src/main/java/com/penekhun/ctfjserver/User/Repository/TokenStorageRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface TokenStorageRepository extends CrudRepository<TokenStorage, Long> {
-    Optional<TokenStorage> findFirstByRefreshTokenAndKeyUsername(String refreshToken, String username);
-    Optional<TokenStorage> findFirstByRefreshToken(String refreshToken);
     Optional<TokenStorage> findByAccessToken(String accessToken);
+    Optional<TokenStorage> findByUsername(String username);
 }
