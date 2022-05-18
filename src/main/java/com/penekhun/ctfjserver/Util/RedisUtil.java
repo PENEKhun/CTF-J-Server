bed1f1f01ca09d853fb3317025e32665e196ac11
penekhun <penekhun@gmail.com>
2022-05-18 21:15:39 +0900

    [RedisUtil] redis 똑같은 유저의 token 데이터 중복 발생 버그 수정

--- src/main/java/com/penekhun/ctfjserver/Util/RedisUtil.java
+++ src/main/java/com/penekhun/ctfjserver/Util/RedisUtil.java
@@ -2,10 +2,14 @@ package com.penekhun.ctfjserver.Util;
 
 import com.penekhun.ctfjserver.User.Entity.TokenStorage;
 import com.penekhun.ctfjserver.User.Repository.TokenStorageRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class RedisUtil {
 
     private final TokenStorageRepository refreshTokenRepository;
@@ -17,7 +21,11 @@ public class RedisUtil {
         this.refreshTokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
     }
 
-    public void insertTokenToStorage(TokenStorage refreshTokenEntity){
+    public void insertTokenToStorage(String username, TokenStorage refreshTokenEntity){
+        //username으로 토큰이 존재하면 삭제하고 등록
+        Optional<TokenStorage> findUser = refreshTokenRepository.findByUsername(username);
+        findUser.ifPresent(tokenStorage -> refreshTokenRepository.deleteById(tokenStorage.getTokenId()));
+
         refreshTokenEntity.setExpiration(refreshTokenValidityInMilliseconds);
         refreshTokenRepository.save(refreshTokenEntity);
     }
