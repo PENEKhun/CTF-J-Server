e842aab35cba43abd3bfa5184e7ab1b32402a524
penekhun <penekhun@gmail.com>
2022-05-21 08:33:23 +0900

    fix : CTF OPEN시간을 controller단 AOP를 통해 확인후 allow, deny처리

---
+++ src/main/java/com/penekhun/ctfjserver/Aop/OpenTimerAOP.java
@@ -0,0 +1,76 @@
+package com.penekhun.ctfjserver.Aop;
+
+import com.penekhun.ctfjserver.Config.CurrentUser;
+import com.penekhun.ctfjserver.Config.Exception.CustomException;
+import com.penekhun.ctfjserver.Config.Exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class OpenTimerAOP {
+
+
+    @Value("${server.enable-open-timer}") boolean enableTimer;
+    @Value("${server.open-time}") String openTime;
+    @Value("${server.open-time-format}") String openTimeFormat;
+    @Value("${server.time-zone}") String timeZone;
+    private final CurrentUser currentUser;
+
+    // User/controller 이하 패키지의 모든 클래스 이하 모든 메서드에 적용
+    @Pointcut("execution(* com.penekhun.ctfjserver.User.Controller..*(..))")
+    private void accessController(){}
+
+    @Before("accessController()")
+    public void checkSiteIsOpen(JoinPoint joinPoint) {
+
+
+        Method method = getMethod(joinPoint);
+
+        if (enableTimer){
+            SimpleDateFormat sdf = new SimpleDateFormat(openTimeFormat);
+
+            try {
+                sdf.setTimeZone(TimeZone.getTimeZone(timeZone));
+            } catch (NullPointerException e){
+                throw new CustomException(ErrorCode.TIME_ZONE_ERROR);
+            }
+
+            try {
+                Date openTimeDate = sdf.parse(openTime);
+                String nowTimeDate = sdf.format(new Date());
+
+                if (!method.getName().startsWith("login") && !currentUser.isAdmin()) {
+                    if (!(openTimeDate.before(sdf.parse(nowTimeDate)))) {
+                        log.info("server not open");
+                        throw new CustomException(ErrorCode.SERVER_NOT_OPEN);
+                    }
+                }
+
+            } catch (ParseException e){
+                throw new CustomException(ErrorCode.OPEN_TIME_ERROR);
+            }
+        }
+    }
+
+    private Method getMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
+    }
+
+}
--- src/main/java/com/penekhun/ctfjserver/Config/Exception/ErrorCode.java
+++ src/main/java/com/penekhun/ctfjserver/Config/Exception/ErrorCode.java
@@ -9,12 +9,16 @@ public enum ErrorCode {
     CANNOT_LOAD_USERNAME(500, "C004", "존재하지 않는 username입니다"),
     DOESNT_EXIST_TOKEN(501, "C005", "존재하지 않는 토큰"),
     ONLY_ACCESS_USER(501, "C006", "유저만 사용 할 수 있는 기능입니다."),
+    SERVER_NOT_OPEN(501, "C007", "서버가 오픈되어있지 않습니다."),
+    OPEN_TIME_ERROR(501, "C008", "open-time format 에러"),
+    TIME_ZONE_ERROR(501, "C009", "time-zone 에러"),
 
     // Member
     EMAIL_DUPLICATION(200, "M001", "존재하는 이메일"),
     NICKNAME_DUPLICATION(200, "M002", "존재하는 닉네임"),
     USERNAME_DUPLICATION(200, "M003", "존재하는 아이디"),
     MEMBER_NOT_FOUND(200, "M004", "존재하지 않은 회원정보"),
+
     //LOGIN_INPUT_INVALID(400, "M002", "Login input is invalid"),
 
     // unchcked
