09fe6cf8a63c9451863ef9be34180140b17dccc5
penekhun <penekhun@gmail.com>
2022-05-20 21:05:01 +0900

    feat S3 파일 업로드를 위한 UploadFileService 코드 구현

---
+++ src/main/java/com/penekhun/ctfjserver/User/Service/UploadFileService.java
@@ -0,0 +1,50 @@
+package com.penekhun.ctfjserver.User.Service;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.penekhun.ctfjserver.FileUpload.UploadFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UploadFileService {
+
+    private final UploadFactory s3Service;
+
+    public String uploadFile(@NotNull MultipartFile file) {
+        String fileName = createFileName(file.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        log.info("attempt file upload : {}", fileName);
+        try (InputStream inputStream = file.getInputStream()) {
+            s3Service.uploadFile(inputStream, objectMetadata, fileName);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(String.format("파일 변환 중 에러가 발생하였습니다 (%s)", file.getOriginalFilename()));
+        }
+        return s3Service.getFileUrl(fileName);
+    }
+
+    // 기존 확장자명을 유지한 채, 유니크한 파일의 이름을 생성하는 로직
+    private String createFileName(String originalFileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
+    }
+
+    // 파일의 확장자명을 가져오는 로직
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 (%s) 입니다", fileName));
+        }
+    }
+}
+
