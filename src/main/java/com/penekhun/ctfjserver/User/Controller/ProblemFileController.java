342af7722590e291adb1008910faf9c00a4e7df2
penekhun <penekhun@gmail.com>
2022-05-20 21:06:35 +0900

    feat File Upload Controller 매핑 부분 구현

--- src/main/java/com/penekhun/ctfjserver/User/Controller/ProblemFileController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/ProblemFileController.java
@@ -1,17 +1,30 @@
 package com.penekhun.ctfjserver.User.Controller;
 
+import com.penekhun.ctfjserver.User.Service.UploadFileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RequestMapping("/api/v1/file")
 @Controller
+@Slf4j
+@RequiredArgsConstructor
 public class ProblemFileController {
+    private final UploadFileService uploadFileService;
 
     @PostMapping("")
-    public void upload(){
-
+    @Secured("ROLE_ADMIN")
+    public ResponseEntity<String> upload(@Valid @NotNull MultipartFile file){
+        return ResponseEntity.ok().body(uploadFileService.uploadFile(file));
     }
 
     @GetMapping
