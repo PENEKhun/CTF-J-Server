ee49092ce39e34f889b369092d47ae01e535a050
penekhun <penekhun@gmail.com>
2022-05-24 07:13:44 +0900

    feat : 캐싱을 위한 Application EnableCaching 어노테이션 추가

--- src/main/java/com/penekhun/ctfjserver/CtfJServerApplication.java
+++ src/main/java/com/penekhun/ctfjserver/CtfJServerApplication.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
