28e0ddaf10b1e2ae25709ed85e8b6073f64ff39f
penekhun <penekhun@gmail.com>
2022-05-24 07:12:28 +0900

    feat : 모든 문제들의 점수를 계산하겠끔 코드 수정

--- src/main/java/com/penekhun/ctfjserver/User/Repository/RankRepository.java
+++ src/main/java/com/penekhun/ctfjserver/User/Repository/RankRepository.java
@@ -32,14 +32,14 @@ public class RankRepository{
         return correctProblemLists;
     }
 
-    public List<ProbListForDynamicScore> findAllProbSolver(){
-//        String sql = "select a" +
-//                "from AuthLog a" +
-//                "where a.problemIdx = p.id and a.isSuccess = true group ";
+    public List<ProbListForDynamicScore> findProbSolver(){
+
         List<Object[]> resultList = em.createQuery("select a.problem.id, a.problem.maxScore, a.problem.minScore, a.problem.solveThreshold, count(a.problem) from AuthLog a where a.isSuccess = true group by a.problem").getResultList();
         //SELECT problem_idx, count(problem_idx) FROM ctf.AuthLog where is_success=true group By problem_idx
-        //0 : problemIdx, 1 : solverCount
-        List<ProbListForDynamicScore> outputList = new ArrayList<ProbListForDynamicScore>();
+        List<Object[]> resultList2 = em.createQuery("select p.id, p.maxScore, p.minScore, p.solveThreshold from Problem p where p.id NOT IN (select a.problem from AuthLog a where a.isSuccess = true)").getResultList();
+        //SELECT idx, max_score, min_score, solve_threshold FROM ctf.Problem where idx NOT IN (SELECT problem_idx FROM ctf.AuthLog WHERE is_success != true)
+
+        List<ProbListForDynamicScore> outputList = new ArrayList<>();
 
         for(Object[] row : resultList){
             ProbListForDynamicScore item = ProbListForDynamicScore.builder()
@@ -50,6 +50,18 @@ public class RankRepository{
                     .solverCount((Long) row[4]).build();
             outputList.add(item);
         }
+
+        for (Object[] row : resultList2) {
+            ProbListForDynamicScore item = ProbListForDynamicScore.builder()
+                    .id((Integer) row[0])
+                    .maxScore((Integer) row[1])
+                    .minScore((Integer) row[2])
+                    .solveThreshold((Integer) row[3])
+                    .solverCount(0L)
+                    .calculatedScore((Integer) row[1]).build();
+            outputList.add(item);
+        }
+
 
         return outputList;
     }
