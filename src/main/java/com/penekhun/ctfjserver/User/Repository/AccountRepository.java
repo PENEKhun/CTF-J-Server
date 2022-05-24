bffb6890a64ac18e0b41cdc95a9c5c6a955852ed
penekhun <penekhun@gmail.com>
2022-05-24 13:28:37 +0900

    fix : Account Repository Id Type 변경

--- src/main/java/com/penekhun/ctfjserver/User/Repository/AccountRepository.java
+++ src/main/java/com/penekhun/ctfjserver/User/Repository/AccountRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Integer> {
     public Optional<Account> findByUsername(String username);
     public Optional<Account> findOneByEmail(String email);
     public Optional<Account> findOneByNickname(String nickname);
