879e8bbde15c1abfa8b1b02337d80b37edd5f221
penekhun <penekhun@gmail.com>
2022-05-24 13:28:12 +0900

    first Commit

--- src/main/java/com/penekhun/ctfjserver/User/Service/RankingService.java
+++ src/main/java/com/penekhun/ctfjserver/User/Service/RankingService.java
@@ -1,8 +1,21 @@
 package com.penekhun.ctfjserver.User.Service;
 
+import com.penekhun.ctfjserver.User.Dto.RankDto;
+import com.penekhun.ctfjserver.Util.RankSchedule;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class RankingService {
 
+    private final RankSchedule rankSchedule;
+
+    public List<RankDto.AccountSolveProbList> getRank(int top){
+        List<RankDto.AccountSolveProbList> accountSolveProbLists = rankSchedule.accountSolveProbLists;
+        return accountSolveProbLists.stream().limit(top).collect(Collectors.toList());
+    }
 }
