42b9d2024bf85ce9787b22a3fc1c79f526f47edb
PENEKhun <penekhun@gmail.com>
2022-06-16 04:49:48 +0900

    refactor : LoginOutController 반환값 수정

--- src/main/java/com/penekhun/ctfjserver/User/Controller/LoginOutController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/LoginOutController.java
@@ -4,11 +4,11 @@ import com.penekhun.ctfjserver.User.Dto.AccountDto;
 import com.penekhun.ctfjserver.User.Dto.TokenDto;
 import com.penekhun.ctfjserver.User.Service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -26,14 +26,16 @@ public class LoginOutController {
     private final AccountService accountService;
 
     @Operation(tags= {"accounts"}, summary = "로그인 API", description = "login API")
-    @ApiResponse(responseCode = "200", description = "OK !!")
+    @ApiResponse(responseCode = "200", description = "~~")
     @PostMapping("login")
+    @ResponseStatus(HttpStatus.OK)
     public TokenDto loginMap(AccountDto.Req.Login login){
         return accountService.login(login);
     }
 
     @Operation(tags= {"accounts"}, summary = "토큰 재발급 API", description = "token reissue API")
     @PostMapping("/reissue")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity reissueMap(@RequestHeader(AUTHORIZATION_HEADER) String accessToken,
                                      @RequestHeader(REFRESH_TOKEN_HEADER) String refreshToken){
         return accountService.reissue(accessToken, refreshToken);
@@ -41,12 +43,14 @@ public class LoginOutController {
 
     @Operation(tags= {"accounts"}, summary = "로그아웃 API", description = "logout API")
     @GetMapping("logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity logoutMap(){
-        return ResponseEntity.ok("logout");
+        return accountService.logout();
     }
 
     @Operation(tags= {"accounts"}, summary = "[테스트용] 회원가입 API", description = "login API for Test")
     @PostMapping("tempSignup")
+    @ResponseStatus(HttpStatus.CREATED)
     public AccountDto.Res.Signup postMapping(@Validated AccountDto.Req.Signup signup){
         //post Account == Signup
         log.info("post account");
