f985d70d79a588f58bd4882f6711f2fd6eebd627
PENEKhun <penekhun@gmail.com>
2022-06-15 21:50:17 +0900

    chore : swagger api docs 생성을 위한 dto 변수 정보 어노테이션 추가

--- src/main/java/com/penekhun/ctfjserver/User/Controller/ProblemController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/ProblemController.java
@@ -8,6 +8,8 @@ import com.penekhun.ctfjserver.User.Entity.Account;
 import com.penekhun.ctfjserver.User.Entity.Problem;
 import com.penekhun.ctfjserver.User.Service.LogService;
 import com.penekhun.ctfjserver.User.Service.ProblemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -25,6 +27,7 @@ import java.util.List;
 @RequestMapping("/api/v1/problem")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "problem", description = "문제 관련 API 모음")
 public class ProblemController {
 
     private final ProblemService problemService;
@@ -32,6 +35,7 @@ public class ProblemController {
     private final ModelMapper modelMapper;
 
     @GetMapping("")
+    @Operation(tags= {"problem"}, summary = "공개된 문제 전체를 가져오는 API", description = "get ALL public Problem API")
     public List<ProblemDto.Res.problemWithoutFlag> getProblemListMapping(){
         List<Problem> problems = problemService.getProblemList();
         List<ProblemDto.Res.problemWithoutFlag> problemsNoFlag = new ArrayList<>();
@@ -42,6 +46,7 @@ public class ProblemController {
 
     //@Secured("ROLE_ADMIN")
     @PostMapping("")
+    @Operation(tags= {"problem"}, summary = "문제 등록하는 API", description = "make Problem API")
     public ProblemDto.Default addProblemMapping(@CurrentUserParameter Account account, @Valid ProblemDto.Default problemDto){
         if (account == null)
             throw new CustomException(ErrorCode.HANDLE_ACCESS_DENIED);
@@ -51,12 +56,14 @@ public class ProblemController {
 
     @Secured("ROLE_ADMIN")
     @DeleteMapping("{problem}")
+    //todo : 문제 삭제 구현
     public ResponseEntity<?> deleteProblemMapping(){
         return null;
     }
 
 
     @PostMapping("{problemId}")
+    @Operation(tags= {"problem"}, summary = "문제 플래그 인증 API", description = "auth FLAG API")
     public ResponseEntity<String> authProblemMapping(@CurrentUserParameter Account account, @PathVariable @Validated @NotNull Integer problemId, @Valid ProblemDto.Req.Auth auth){
         boolean isCorrect = problemService.authProblem(account, problemId, auth);
         logService.authProblemLog(problemId, auth.getFlag(), isCorrect);
@@ -66,6 +73,7 @@ public class ProblemController {
     }
 
     @GetMapping("{category}")
+    @Operation(tags= {"problem"}, summary = "카테고리를 통해 문제 리스트를 가져오는 API", description = "get problem from category API")
     public List<ProblemDto.Res.problemWithoutFlag> getProblemFromCategoryMapping(@PathVariable @Validated @NotNull String category){
         List<Problem> problems = problemService.getProblemListFromCategory(category);
         List<ProblemDto.Res.problemWithoutFlag> problemsNoFlag = new ArrayList<>();
