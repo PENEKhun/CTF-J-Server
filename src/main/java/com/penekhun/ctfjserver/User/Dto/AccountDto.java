88e907315f27d44b045b06bf3c9952600d4d9745
PENEKhun <penekhun@gmail.com>
2022-06-15 15:42:25 +0900

    chore : swagger api docs 생성을 위한 dto 변수 정보 어노테이션 추가

--- src/main/java/com/penekhun/ctfjserver/User/Dto/AccountDto.java
+++ src/main/java/com/penekhun/ctfjserver/User/Dto/AccountDto.java
@@ -1,5 +1,7 @@
 package com.penekhun.ctfjserver.User.Dto;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -16,19 +18,28 @@ public class AccountDto {
         @Data
         @Valid
         public static class Signup{
+            @Schema(description = "3~30자사이 아이디")
+            @ApiModelProperty(value = "3~30자사이 아이디", required = true)
             @Length(min = 3, max = 30)
             private String username;
 
+            @Schema(description = "8~20사이 패스워드")
+            @ApiModelProperty(value = "8~20사이 패스워드", required = true)
             @Length(min = 8, max = 20)
             private String password;
 
+            @Schema(description = "3~20자 사이 닉네임")
+            @ApiModelProperty(value = "3~20자 사이 닉네임", required = true)
             @Length(min = 3, max = 20)
             private String nickname;
 
             @Email
+            @Schema(description = "이메일")
+            @ApiModelProperty(value = "이메일", required = true)
             private String email;
 
             @Length(min = 2, max = 10)
+            @ApiModelProperty(value = "2~10자 사이 실명", required = true)
             private String realName;
 
             @Builder
