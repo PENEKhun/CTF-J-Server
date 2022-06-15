3c8b15eb306be02ba41b930b9718b83b74d9b7c0
PENEKhun <penekhun@gmail.com>
2022-06-15 21:50:45 +0900

    first commit

--- src/main/java/com/penekhun/ctfjserver/User/Controller/ConfigController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/ConfigController.java
@@ -1,14 +1,26 @@
 package com.penekhun.ctfjserver.User.Controller;
 
+import com.penekhun.ctfjserver.Aop.OpenTimerAOP;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/config")
 @Slf4j
 @RequiredArgsConstructor
 public class ConfigController {
+    private final OpenTimerAOP openTimerAOP;
+
+    @GetMapping("")
+    public Collection<String> configMapping(){
+        //todo: openTime, endTime Config Data 출력
+        //openTimerAOP.getOpenTimeFormat()
+        return null;
+    }
 
 }
