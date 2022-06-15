4b3c7ea98cd64e74103cb3755c63c7ca153cee99
PENEKhun <penekhun@gmail.com>
2022-06-15 15:43:20 +0900

    chore : LoginOutController.java swagger api 그룹화

--- src/main/java/com/penekhun/ctfjserver/User/Controller/LoginOutController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/LoginOutController.java
@@ -3,6 +3,10 @@ package com.penekhun.ctfjserver.User.Controller;
 import com.penekhun.ctfjserver.User.Dto.AccountDto;
 import com.penekhun.ctfjserver.User.Dto.TokenDto;
 import com.penekhun.ctfjserver.User.Service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -16,29 +20,35 @@ import static com.penekhun.ctfjserver.Config.Jwt.JwtFilter.REFRESH_TOKEN_HEADER;
 @RequestMapping("/api/v1/")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "accounts", description = "계정(로그인)관련 API 모음")
 public class LoginOutController {
 
     private final AccountService accountService;
 
+    @Operation(tags= {"accounts"}, summary = "로그인 API", description = "login API")
+    @ApiResponse(responseCode = "200", description = "OK !!")
     @PostMapping("login")
     public TokenDto loginMap
-            (@RequestParam String username,
-             @RequestParam String password){
+            (@Parameter(description = "아이디", required = true) @RequestParam String username,
+             @Parameter(description = "비밀번호", required = true) @RequestParam String password){
 
         return accountService.login(username, password);
     }
 
+    @Operation(tags= {"accounts"}, summary = "토큰 재발급 API", description = "token reissue API")
     @PostMapping("/reissue")
     public ResponseEntity reissueMap(@RequestHeader(AUTHORIZATION_HEADER) String accessToken,
                                      @RequestHeader(REFRESH_TOKEN_HEADER) String refreshToken){
         return accountService.reissue(accessToken, refreshToken);
     }
 
+    @Operation(tags= {"accounts"}, summary = "로그아웃 API", description = "logout API")
     @GetMapping("logout")
     public ResponseEntity logoutMap(){
         return ResponseEntity.ok("logout");
     }
 
+    @Operation(tags= {"accounts"}, summary = "[테스트용] 회원가입 API", description = "login API for Test")
     @PostMapping("tempSignup")
     public AccountDto.Res.Signup postMapping(@Validated AccountDto.Req.Signup signup){
         //post Account == Signup
@@ -47,4 +57,7 @@ public class LoginOutController {
         return accountService.signup(signup);
     }
 
+    public Object loginMap() {
+        return this.loginMap().getClass();
+    }
 }
