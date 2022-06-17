27bd133c25f002c29c4ee2b115a961cdfc824e46
PENEKhun <penekhun@gmail.com>
2022-06-17 18:00:25 +0900

    first commit

--- src/main/java/com/penekhun/ctfjserver/User/Controller/AccountController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/AccountController.java
@@ -1,14 +1,15 @@
 package com.penekhun.ctfjserver.User.Controller;
 
+import com.penekhun.ctfjserver.Config.CurrentUserParameter;
 import com.penekhun.ctfjserver.User.Dto.AccountDto;
+import com.penekhun.ctfjserver.User.Entity.Account;
 import com.penekhun.ctfjserver.User.Service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/account")
@@ -18,12 +19,28 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    //todo : only access for admin
     @PostMapping("")
-    public AccountDto.Res.Signup postMapping(@RequestBody @Validated AccountDto.Req.Signup signup){
-        //post Account == Signup
-        log.info("post account");
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(tags= {"accounts"}, summary = "회원가입 API", description = "signup API")
+    public AccountDto.Res.Signup signupMapping(@Validated AccountDto.Req.Signup signup) {
         return accountService.signup(signup);
     }
+
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(tags= {"accounts"}, summary = "나의 회원정보 가져오는 API", description = "lookup my account")
+    public AccountDto.Res.MyPage lookupMyAccountMapping(@CurrentUserParameter Account account){
+        return accountService.getMyAccount(account);
+    }
+
+    @PatchMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(tags= {"accounts"}, summary = "나의 회원정보의 일부를 수정하는 API", description = "edit my account partly")
+    public AccountDto.Res.Signup editMyAccountPartlyMapping(@CurrentUserParameter Account account, AccountDto.Req.Signup signup){
+        return accountService.editMyAccountPartly(signup);
+    }
+
 
 
 
