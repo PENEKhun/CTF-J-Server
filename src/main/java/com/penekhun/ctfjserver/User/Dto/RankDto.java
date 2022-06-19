71c3442890111d50dbd4585abcab71049c85492b
PENEKhun <penekhun@gmail.com>
2022-06-19 15:27:38 +0900

    fix : Swagger Docs사용을 위해서 Dto에 스키마 작성

--- src/main/java/com/penekhun/ctfjserver/User/Dto/RankDto.java
+++ src/main/java/com/penekhun/ctfjserver/User/Dto/RankDto.java
@@ -1,5 +1,6 @@
 package com.penekhun.ctfjserver.User.Dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -37,10 +38,15 @@ public class RankDto {
     @Getter
     @AllArgsConstructor
     public static class AccountSolveProbList {
+        @Schema(description = "계정 인덱스")
         private Integer accountId;
+        @Schema(description = "유저 닉네임")
         private String nickname;
+        @Schema(description = "푼 문제 리스트", example = "{int, int, int, }")
         private List<Integer> probIdList;
+        @Schema(description = "마지막으로 문제를 맞춘 시간", example = "timestamp")
         private Timestamp lastAuthTime;
+        @Schema(description = "점수", example = "timestamp")
         private int score = 0;
 
         public void addScore(Integer score) {
