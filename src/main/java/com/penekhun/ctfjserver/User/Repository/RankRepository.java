28036198c63717767c0ad84998c0d4d8d68f012f
PENEKhun <penekhun@gmail.com>
2022-06-22 18:10:49 +0900

    refactor : Polling 관련 코드들 리팩터링

--- src/main/java/com/penekhun/ctfjserver/User/Repository/RankRepository.java
+++ src/main/java/com/penekhun/ctfjserver/User/Repository/RankRepository.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
