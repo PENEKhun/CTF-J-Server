770963b668706ce54d5f6261bba615c56a70bee1
PENEKhun <penekhun@gmail.com>
2022-06-22 22:44:45 +0900

    fix : rank api 역할 분리 : /api/v1/rank/{} 에서 현재 랭크와 과거 랭크를 동시에 띄우던걸 2개의 api로 역할을 나누었습니다.

--- src/main/java/com/penekhun/ctfjserver/User/Controller/RankingController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/RankingController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,11 +23,19 @@ public class RankingController {
     private final RankingService rankingService;
 
     @GetMapping("{howMany}")
-    @Operation(tags= {"rank"}, summary = "랭킹을 가져오는 API", description = "인자로 넘겨준 숫자 만큼 랭크를 가져옵니다.")
+    @Operation(tags= {"rank"}, summary = "현재 랭킹을 가져오는 API", description = "인자로 넘겨준 숫자 만큼 랭크를 가져옵니다.")
     @Parameter(name = "howMany", description = "가져올 상위 n등", example = "5", required = true)
-    public RankDto.EveryHourScore getRank(
-             @PathVariable @Valid @NotNull Integer howMany){
+    public List<RankDto.AccountSolveProbList> getRank(
+            @PathVariable @Valid @NotNull Integer howMany){
         return rankingService.getRank(howMany);
+    }
+
+    @GetMapping("/history/{howMany}")
+    @Operation(tags= {"rank"}, summary = "랭킹 차트 그릴때 필요한 API", description = "상위 {howMany}등들의 과거 점수들을 가져옵니다.")
+    @Parameter(name = "howMany", description = "상위 n등", example = "5", required = true)
+    public List<RankDto.RankWithTimestamp> getRankHistory(
+            @PathVariable @Valid @NotNull Integer howMany){
+        return rankingService.getRankHistory(howMany);
     }
 
 
