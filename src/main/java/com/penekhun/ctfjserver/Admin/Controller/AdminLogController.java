c0ad835c469f7f168c8876a919cae2509c07de23
PENEKhun <penekhun@gmail.com>
2022-06-24 18:22:30 +0900

    first Commit

--- src/main/java/com/penekhun/ctfjserver/Admin/Controller/AdminLogController.java
+++ src/main/java/com/penekhun/ctfjserver/Admin/Controller/AdminLogController.java
@@ -1,7 +1,14 @@
 package com.penekhun.ctfjserver.Admin.Controller;
 
-import com.penekhun.ctfjserver.User.Service.RankingService;
+import com.penekhun.ctfjserver.Admin.Dto.LogDto;
+import com.penekhun.ctfjserver.Admin.Service.AdminLogService;
+import com.penekhun.ctfjserver.Config.Exception.CustomException;
+import com.penekhun.ctfjserver.Config.Exception.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,13 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Secured("ROLE_ADMIN")
 public class AdminLogController {
-    private final RankingService rankingService;
+    private final AdminLogService adminLogService;
 
-    @GetMapping("flag/{currentPageNo}/{recordsPerPage}")
+    @GetMapping("")
     @Operation(security = { @SecurityRequirement(name = "bearer-key")},
-            tags= {"admin.log"}, summary = "플래그 인증 로그 API - 미완", description = "플래그 인증 시도한 로그를 보여주는 API")
-    public void getAuthLog(){
+            tags= {"admin.log"}, summary = "로그 API", description = "로그를 보여주는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = LogDto.Res.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값", ref = "#/components/responses/ErrorCode.INVALID_INPUT_VALUE")})
+    public LogDto.Res getLogMapping(LogDto.Req req){
+
+        if (req.getLogType().equalsIgnoreCase("FLAG")) {
+            return adminLogService.getAuthLog(req);
+        } else if (req.getLogType().equalsIgnoreCase("LOG")) {
+            return adminLogService.getLog(req);
+        } else
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
     }
+
 
 
 }
