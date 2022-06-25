fc44ad61e2afbf387e47ee4904dddea76e3b7180
PENEKhun <penekhun@gmail.com>
2022-06-25 14:46:45 +0900

    style : 관리자 파일 서비스 변수명 변경

--- src/main/java/com/penekhun/ctfjserver/Admin/Controller/AdminProblemFileController.java
+++ src/main/java/com/penekhun/ctfjserver/Admin/Controller/AdminProblemFileController.java
@@ -1,5 +1,6 @@
 package com.penekhun.ctfjserver.Admin.Controller;
 
+import com.penekhun.ctfjserver.Admin.Service.AdminFileService;
 import com.penekhun.ctfjserver.Config.Exception.CustomException;
 import com.penekhun.ctfjserver.Config.Exception.ErrorCode;
 import com.penekhun.ctfjserver.User.Dto.ProblemDto;
