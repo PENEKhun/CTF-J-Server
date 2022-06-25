d4596581f18fbd67355fe3c19239378286637059
PENEKhun <penekhun@gmail.com>
2022-06-25 14:45:00 +0900

    first Commit

---
+++ src/main/java/com/penekhun/ctfjserver/Admin/Service/AdminNoticeService.java
@@ -0,0 +1,21 @@
+package com.penekhun.ctfjserver.Admin.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AdminNoticeService {
+
+    public void removeNotice(Integer noticeId){
+
+    }
+
+    public void postNotice(){
+
+    }
+
+
+}
