b4d85574eb7739eb631bae34005db0d4668befd0
PENEKhun <penekhun@gmail.com>
2022-06-25 14:47:55 +0900

    feat : FileManagement 인터페이스에 파일 삭제 메서드 작성

--- src/main/java/com/penekhun/ctfjserver/FileUpload/FileManagement.java
+++ src/main/java/com/penekhun/ctfjserver/FileUpload/FileManagement.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 
 public interface FileManagement {
     void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName);
+    void deleteFile(String key);
     String getFileUrl(String fileName);
     ResponseEntity<byte[]> downloadFile(String storedFileName) throws IOException;
 }
