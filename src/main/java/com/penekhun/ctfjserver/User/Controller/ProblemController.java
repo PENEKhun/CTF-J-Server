76962698185a5b81eb6aff1ca4f17f2a771fccf2
PENEKhun <penekhun@gmail.com>
2022-07-13 11:12:33 +0900

    style : clean code

--- src/main/java/com/penekhun/ctfjserver/User/Controller/ProblemController.java
+++ src/main/java/com/penekhun/ctfjserver/User/Controller/ProblemController.java
@@ -21,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
